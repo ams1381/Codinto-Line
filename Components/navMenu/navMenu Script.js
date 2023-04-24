04e3fdb3cc9636cd43a8e3eba882a89ca0c476df
AMS <amirmohammadsolgi1381@gmail.com>
2023-04-24 23:29:24 +0430

    Added Script to Navbar Component

---
+++ Components/navMenu/navMenu Script.js
@@ -0,0 +1,16 @@
+const nav_menu = document.querySelector(".navMenu");
+const nav_menu_toggle = document.querySelector(".navToggleButton");
+const nav_mask = document.querySelector(".screen-mask");
+
+const menu_toggle_handler = () => {
+    nav_menu.classList.toggle("active");
+    nav_mask.classList.toggle("active");
+}
+const mask_close_panel = () => {
+    if(nav_menu.classList.contains("active"))
+         nav_menu.classList.remove("active");
+    nav_mask.classList.remove("active");  
+}
+
+nav_menu_toggle.addEventListener("click" , menu_toggle_handler);
+nav_mask.addEventListener("click" , mask_close_panel);
