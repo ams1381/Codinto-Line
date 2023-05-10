10b6e7dfd02ee96169f6668d4bd1c44a35f5488e
yassin-bera <yassinbeyrami7@gmail.com>
2023-05-10 15:25:53 +0330

    questionBox5

---
+++ Components/questionBox/rangeSelect.js
@@ -0,0 +1,10 @@
+const rangeSelect = document.querySelector('.range__select');
+console.log(rangeSelect);
+rangeSelect.addEventListener('click', (e) => {
+    // console.log(e.target.classList.value);
+    if (e.target.classList.value === 'range__number') {
+        e.target.classList.add('range__active');
+    }else if(e.target.classList.contains('range__active')){
+        e.target.classList.remove('range__active');
+    }
+});
