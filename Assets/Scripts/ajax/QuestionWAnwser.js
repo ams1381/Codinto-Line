19c176f3040d854c71c57207b8830546e2998521
yassin-bera <yassinbeyrami7@gmail.com>
2023-05-16 15:35:43 +0330

    js ajax

---
+++ Assets/Scripts/ajax/QuestionWAnwser.js
@@ -0,0 +1,10 @@
+import {getRequest , baseUrl} from "./ajax/ajaxRequsts.js";
+
+
+
+
+const reqUrl = baseUrl +"/question-api/questionnaires/e4f2305e-d282-4417-9484-e429f9a661a2/textanswer-questions/"
+
+getRequest(reqUrl).then((res) => {
+    console.log(res.data)
+})
---
+++ Assets/Scripts/ajax/ajaxRequsts.js
@@ -0,0 +1,31 @@
+
+const baseUrl = "http://codinto-line-flash.web97.ir"
+async function getRequest(url){
+        return axios.get(url, {
+            headers: {
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg0NTY1MTQ5LCJqdGkiOiI2ZDk2MTZlODY2ZjA0ZTNjYjU5ODgyODRmMDkwZjZmMiIsInVzZXJfaWQiOjF9.Htd-3K2LmyOtF3rPcpd9pc2Xez-fMe4g8UvMFGNKRJM"
+            }
+        });
+}
+async function patchRequest(url){
+    return axios.patch(url, {
+        headers: {
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg0NTY1MTQ5LCJqdGkiOiI2ZDk2MTZlODY2ZjA0ZTNjYjU5ODgyODRmMDkwZjZmMiIsInVzZXJfaWQiOjF9.Htd-3K2LmyOtF3rPcpd9pc2Xez-fMe4g8UvMFGNKRJM"
+        }
+    });
+}
+async function postRequest(url){
+    return axios.post(url, {
+        headers: {
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg0NTY1MTQ5LCJqdGkiOiI2ZDk2MTZlODY2ZjA0ZTNjYjU5ODgyODRmMDkwZjZmMiIsInVzZXJfaWQiOjF9.Htd-3K2LmyOtF3rPcpd9pc2Xez-fMe4g8UvMFGNKRJM"
+        }
+    });
+}
+async function deleteRequest(url){
+    return axios.delete(url, {
+        headers: {
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg0NTY1MTQ5LCJqdGkiOiI2ZDk2MTZlODY2ZjA0ZTNjYjU5ODgyODRmMDkwZjZmMiIsInVzZXJfaWQiOjF9.Htd-3K2LmyOtF3rPcpd9pc2Xez-fMe4g8UvMFGNKRJM"
+        }
+    });
+}
+export {getRequest ,    patchRequest ,    postRequest ,    deleteRequest , baseUrl}
